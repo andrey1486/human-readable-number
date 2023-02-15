4b961972523d4092c3187451e35e3336c23b55a2
Andrey <88458447+andrey1486@users.noreply.github.com>
2023-02-15 11:37:05 +0200

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function toReadable (number) {
-  
+  module.exports = function toReadable (number) {
+      
+  const NumbersArray = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  const dozensArray = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+  if (number < 20) {  
+    return NumbersArray[number]
+  } else if (number < 100 && !(number % 10)) {
+    return dozensArray[Math.floor(number / 10)]
+  } else if (number < 100) {
+    return dozensArray[Math.floor(number / 10)] + ' ' + NumbersArray[(number % 10)]
+  } else if ( !(number % 100)) {
+    return NumbersArray[(Math.floor(number / 100))] + ' hundred'
+  } else if (number % 100 < 20) {
+    return NumbersArray[Math.floor(number / 100)] + ' hundred ' + NumbersArray[(number % 100)]
+  } else if (!(number % 10)) {
+    return NumbersArray[Math.floor(number / 100)] + ' hundred ' + dozensArray[Math.floor((number % 100) / 10)]
+  } else {
+    return NumbersArray[Math.floor(number / 100)] + ' hundred ' + dozensArray[Math.floor((number % 100) / 10)] + ' ' + NumbersArray[(number % 10)]
+  }
+      
 }
